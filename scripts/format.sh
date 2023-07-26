064aa9dbe04d6cd3f419c3f7b442e87885fcfc0b
zhangwei <wei1157@hotmail.com>
2023-07-26 20:51:20 +0800

    skip grpc file when unifying code styles

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place src/ --exclude=__init__.py
-black src/
-isort src/
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place src/ --exclude='__init__.py,*_pb2.py,*_pb2_grpc.py'
+black --exclude='.*_pb2_grpc.py|.*_pb2.py|.*_pb2.pyi' src/
+isort --skip=chatglm_pb2.py --skip=chatglm_pb2.pyi --skip=chatglm_pb2_grpc.py src/
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-mypy src/
-black --check src/
-isort --check-only src/
-flake8 --max-line-length=88 --exclude=__init__.py
+mypy --exclude='grpc_pkg' src/
+black --check --exclude='.*_pb2_grpc.py|.*_pb2.py|.*_pb2.pyi' src/
+isort --check-only --skip=chatglm_pb2.py --skip=chatglm_pb2.pyi --skip=chatglm_pb2_grpc.py src/
+flake8 --max-line-length=88 --extend-ignore=E203 --exclude='__init__.py,*_pb2.py,*_pb2_grpc.py,*_pb2.pyi'
