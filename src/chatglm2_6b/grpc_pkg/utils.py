ccfc7798094401c3f093ce8da3df2340cc657831
zhangwei <wei1157@hotmail.com>
2023-07-27 20:14:50 +0800

    fix: wrong chat history type when return to gradio component

--- src/chatglm2_6b/grpc_pkg/utils.py
+++ src/chatglm2_6b/grpc_pkg/utils.py
@@ -2,7 +2,7 @@ from chatglm2_6b.grpc_pkg import chatglm_pb2
 
 
 def history2list(history: chatglm_pb2.ChatHistory):
-    return [(s for s in turn.strings) for turn in history.turns]
+    return [[s for s in turn.strings] for turn in history.turns]
 
 
 def list2history(str_list):
